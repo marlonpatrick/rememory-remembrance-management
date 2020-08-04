a1e4b58d70fa26763f599d123291e52cc7bdf648
Marlon Patrick <marlon.patrick@cesar.org.br>
2020-08-04 01:47:13 -0300

    feat: create marker interface to delimit ComponentScan package

---
+++ memories-management-app/memories-management-common/src/main/java/io/mpwtech/randommemories/ComponentScanBasePackageClass.java
@@ -0,0 +1,5 @@
+package io.mpwtech.randommemories;
+
+public interface ComponentScanBasePackageClass {
+
+}
---
+++ memories-management-app/memories-management-rest/src/main/java/io/mpwtech/randommemories/memoriesmanagement/rest/MemoriesManagementRestApplication.java
@@ -0,0 +1,18 @@
+package io.mpwtech.randommemories.memoriesmanagement.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import io.mpwtech.randommemories.ComponentScanBasePackageClass;
+
+@ServletComponentScan
+@SpringBootApplication
+@ComponentScan(basePackageClasses = {ComponentScanBasePackageClass.class})
+public class MemoriesManagementRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriesManagementRestApplication.class, args);
+	}
+}
+
