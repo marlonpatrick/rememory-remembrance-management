910c9e3768c85913e8e9f2a4250a79db87a5a0bf
Marlon Patrick <marlonpatrick10@gmmail.com>
2020-11-26 01:12:55 -0300

    chore: refactors

--- remembrance-management-rest/src/main/java/io/mpwtech/rememory/remembrancemanagement/rest/remembrance/TestMongoStreamingAppRunner.java
+++ remembrance-management-rest/src/main/java/io/mpwtech/rememory/remembrancemanagement/rest/remembrance/TestMongoStreamingAppRunner.java
@@ -1,14 +1,8 @@
 package io.mpwtech.rememory.remembrancemanagement.rest.remembrance;
 
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.addFields;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
-import static org.springframework.data.mongodb.core.query.Criteria.matchingDocumentStructure;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
-import static org.springframework.data.mongodb.core.schema.JsonSchemaProperty.array;
-import static org.springframework.data.mongodb.core.schema.JsonSchemaProperty.object;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.changestream.FullDocument;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +11,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.data.mongodb.core.ChangeStreamEvent;
 import org.springframework.data.mongodb.core.ChangeStreamOptions;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.schema.JsonSchemaObject.Type;
-import org.springframework.data.mongodb.core.schema.MongoJsonSchema;
 import reactor.core.publisher.Flux;
 
 public class TestMongoStreamingAppRunner implements ApplicationRunner {
